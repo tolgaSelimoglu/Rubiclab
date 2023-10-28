3cb95234d88723228043b746200a1010bc8fc950
MulahTeni <melihtunaipek.mti@gmail.com>
2023-10-28 15:45:31 +0300

    magic

--- AI/solve_rub.py
+++ AI/solve_rub.py
@@ -1,61 +1,7 @@
-"""
-    inputs:
-        cubestring :
-            The cube color positions obtained by processing the captured images should be
-             in the following format: 'UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB'
-            U : Up
-            R : Right
-            F : Front
-            D : Down
-            L : Left
-            B : Back
-        t:
-            Time to solve the cube, if could not find a solution on time find the closest time (seconds)
-        m:
-            Exact number of moves to solve the cube, enter 0 for the shortest maneuver found in this time t
+import sys
+import os
+from src.code.modules import *
 
 
-        example of usage:
-
-            cubestring = 'DUUBULDBFRBFRRULLLBRDFFFBLURDBFDFDRFRULBLUFDURRBLBDUDL'
-            t = 2
-            m = 19
-            sv.solve(cubestring, m, t)
-
-    --------------------------------------------------------------------------------------------------------
-
-    output:
-        String that contains the moves to solve the cube and at the end the number of moves.
-
-        example of output:
-
-            >>> L3 U1 B1 R2 F3 L1 F3 U2 L1 U3 B3 U2 B1 L2 F1 U2 R2 L2 B2 (19f)
-
-            The algorithm output is given as the face to be rotated and
-             the number of degrees to be rotated clockwise divided by 90.
-
-
-    some cube examples:
-
-        FFDFUUBBDRBFLRRLUURDBFFUBBBURDBDFFDLULURLDDLLLRRUBDFLR (16 moves in 5 seconds)
-        FDDLURBFDRFRLRDFBRLDBRFFBBLURDBDLLFBUUDDLUFULFBRLBRUUU (18 moves in 5 seconds)
-        BLRBUDDLBUFULRUURBBDLDFBDLLLUFUDFRURRRLDLBFRFFFUBBRDFD (18 moves in 5 seconds)
-        DULDULDDLFUBDRFBBRRRDDFBURLFUURDLRBUFLBLLFBBLDFRRBFFUU (19 moves in 5 seconds)
-        DBDBUFURUBLFBRLFFLFDRRFUBLDDDRBDURUUBDLDLFULRLRLUBFBRF (20 moves in 5 seconds)
-        RBBRURRDLDFLLRUUFDDLBFFFRDLUBFBDUFBFBDFRLDRUBULDLBULRU (19 moves in 5 seconds)
-        UDLRURDBBDFUBRLFDRBLRBFUFLURULRDBFDUBDLFLRLUDBFRFBUFLD (18 moves in 5 seconds)
-        BRFRULFUBUFLLRBBRRULLLFBBDDDFLBDULRFRDRULDFURDFUDBFDBU (19 moves in 5 seconds)
-        FUBFUBBBDRLDFRFBDRURBBFUUDUFLRUDRFFURLLULDLRLLBDRBLFDD (19 moves in 5 seconds)
-        BRLLUFLDRBUUURLDUFURDDFBLLFDFLLDRRFRRUFBLFFBBBBUDBDURD (20 moves in 5 seconds)
-
-"""
-
-import twophase.solver as sv
-
-cubestring = 'Cube to Solve'
-t = 5
-m = 0
-
-solution = sv.solve(cubestring, m, t)
-
-print(solution)
+if __name__ == "__main__":
+    solve("src/data/cube_colours.txt", "src/data/moves_encoded.txt")
---
+++ AI/src/code/modules.py
@@ -0,0 +1,46 @@
+import twophase.solver as sv
+
+
+def moves_to_binary(moves, target_path):
+
+    encoded_lines = []
+
+    dict = {
+            'U1' : '00001', 'U2' : '00010', 'U3' : '00011',
+            'D1' : '00101', 'D2' : '00110', 'D3' : '00111',
+            'L1' : '01001', 'L2' : '01010', 'L3' : '01011',
+            'R1' : '01101', 'R2' : '01110', 'R3' : '01111',
+            'F1' : '10001', 'F2' : '10010', 'F3' : '10011',
+            'B1' : '11001', 'B2' : '11010', 'B3' : '11011'
+            }
+
+
+    for move in moves:
+        encoded_lines.append(dict[move[:2]])
+
+    with open(target_path, 'w') as f:
+        for move in encoded_lines:
+            f.write(f'{move}\n')
+
+
+def predict_moves(path, moves = 0, time = 5):
+
+    cubestring = ''
+    with open(path, 'r') as f:
+        cubestring = f.read()
+
+    cubestring = cubestring[0:54]
+
+    solution = sv.solve(cubestring, moves, time)
+
+    sol_moves = solution.split(" ")
+
+    move_count = len(sol_moves) - 1
+
+    return sol_moves[:move_count]
+
+
+def solve(path, target_path):
+
+    moves = predict_moves(path)
+    moves_to_binary(moves, target_path)

f25c34fc99d4f691da50c6bc3b5e07ff79455415
MulahTeni <melihtunaipek.mti@gmail.com>
2023-11-07 18:31:05 +0300

    change path to faces.txt file

--- AI/solve_rub.py
+++ AI/solve_rub.py
@@ -4,4 +4,4 @@ from src.code.modules import *
 
 
 if __name__ == "__main__":
-    solve("src/data/cube_colours.txt", "src/data/moves_encoded.txt")
+    solve("src/data/faces.txt", "src/data/moves_encoded.txt")

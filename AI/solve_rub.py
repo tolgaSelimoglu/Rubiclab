cf44b1418d3b8517ba7d50963941c0557583f63d
MulahTeni <melihtunaipek.mti@gmail.com>
2023-11-10 19:30:31 +0300

    main

--- AI/solve_rub.py
+++ AI/solve_rub.py
@@ -2,6 +2,8 @@ import sys
 import os
 from src.code.modules import *
 
+def main():
+    solve("src/data/cube_colours.txt", "src/data/moves_encoded.txt")
 
 if __name__ == "__main__":
-    solve("src/data/cube_colours.txt", "src/data/moves_encoded.txt")
+    main()
